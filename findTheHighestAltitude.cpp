ca8f1df4ffc6bb04ec6cf3778457a0e5ee60fff6
shezan2004 <shezantouhidulalam@gmail.com>
2026-06-19 09:30:48 +0600

    Add solution to find the highest altitude from altitude gains

---
+++ findTheHighestAltitude.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    int largestAltitude(std::vector<int>& gain) {
+        int max_altitude = 0;
+        int current_altitude = 0;
+        
+        for (int i = 0; i < gain.size(); ++i) {
+            current_altitude += gain[i];
+            max_altitude = std::max(max_altitude, current_altitude);
+        }
+        
+        return max_altitude;
+    }
+};
